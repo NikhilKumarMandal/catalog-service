1743e381263894c8cfa64200f9e0f53a4396f6e7
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-09-27 12:22:08 +0530

    add async wrapper

--- src/category/category.routes.ts
+++ src/category/category.routes.ts
@@ -3,7 +3,11 @@ import { CategoryController } from "./category.controller";
 import categoryValidator from "./category.validator";
 import { CategoryService } from "./category.service";
 import logger from "../config/logger";
+import { asyncWrapper } from "../utils";
+;
 const router = express.Router()
+
+
 
 
 const categoryService = new CategoryService()
@@ -12,7 +16,7 @@ const categoryController = new CategoryController(categoryService,logger)
 
 
 
-router.post("/",categoryValidator,categoryController.create)
+router.post("/",categoryValidator,asyncWrapper(categoryController.create))
 
 
 export default router;
--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,15 @@
+import createHttpError from "http-errors"
+import { NextFunction, RequestHandler, Request, Response } from "express"
+
 export const DB_NAME = "CATALOG_SERVICE"
+
+export const asyncWrapper = (requestHandler: RequestHandler) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => {
+            if (err instanceof Error) {
+                return next(createHttpError(500,err.message))
+            }
+            return next(createHttpError(500,"Internal Server Error"))
+        })
+    }
+}
